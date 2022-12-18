601e9c7e9cfbb9eeb3c782aeac0174ae6e910589
Darsa <mohamad.darsa@foo-solutions.com>
2022-12-19 00:10:44 +0200

    phone factory using dynamic functions

--- jmlibrary/src/main/java/web/gdsc/jmlibrary/PhoneFactory.java
+++ jmlibrary/src/main/java/web/gdsc/jmlibrary/PhoneFactory.java
@@ -3,32 +3,19 @@ package web.gdsc.jmlibrary;
 import web.gdsc.jmlibrary.phone.Phone;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class PhoneFactory {
 
+    private final Map<String, Supplier<Phone>> phoneMap = new HashMap<>();
+
     public Phone getPhone(String phoneType) {
-        if(phoneType.equalsIgnoreCase("A53")) {
-            Phone a53 = new Phone();
-            a53.setType("A53");
-            a53.setOperatingSystem("android 11");
-            a53.setReleaseDate(LocalDate.of(2022, 3, 24));
-            a53.setPrice(449.99);
-            a53.setResY(2400.0);
-            a53.setResX(1080.0);
-            return a53;
-        }
+        return phoneMap.getOrDefault(phoneType, () -> null).get();
+    }
 
-        else if(phoneType.equalsIgnoreCase("iphone 13")) {
-            Phone iphone13 = new Phone();
-            iphone13.setType("iphone 13");
-            iphone13.setOperatingSystem("IOS 15");
-            iphone13.setReleaseDate(LocalDate.of(2021, 9, 24));
-            iphone13.setPrice(799.99);
-            iphone13.setResY(2532.0);
-            iphone13.setResX(1170.0);
-            return iphone13;
-        }
-
-        else return null;
+    public void setPhoneConstructor(String phoneType, Supplier<Phone> supplier) {
+        phoneMap.put(phoneType, supplier);
     }
 }
--- jmproject/src/main/java/web/gdsc/jmproject/ProjectDriver.java
+++ jmproject/src/main/java/web/gdsc/jmproject/ProjectDriver.java
@@ -1,10 +1,28 @@
 package web.gdsc.jmproject;
 
 import web.gdsc.jmlibrary.PhoneFactory;
+import web.gdsc.jmlibrary.phone.Phone;
+
+import java.time.LocalDate;
 
 public class ProjectDriver {
     public static void main(String[] args) {
         PhoneFactory phoneFactory = new PhoneFactory();
-        System.out.println(phoneFactory.getPhone("Iphone 13"));
+
+        //registered a function that creates an A53 phone
+        phoneFactory.setPhoneConstructor("A53", ProjectDriver::createA53);
+
+        System.out.println(phoneFactory.getPhone("A523"));
+    }
+
+    public static Phone createA53() {
+        Phone phone = new Phone();
+        phone.setType("A53");
+        phone.setResX(1080.0);
+        phone.setPrice(399.99);
+        phone.setResY(2400.0);
+        phone.setReleaseDate(LocalDate.of( 2022, 3, 24));
+        phone.setOperatingSystem("android 11");
+        return phone;
     }
 }
